b8a5b05cd602593fe22cc5892b7f7325d24a72b8
“ervinse” <ervinse@foxmail.com>
2022-10-20 11:15:02 +0800

    v 0.0.2 添加配置文件

--- src/test/java/pers/ervinse/HomeworkManagementSystemApplicationTests.java
+++ src/test/java/pers/ervinse/HomeworkManagementSystemApplicationTests.java
@@ -3,11 +3,14 @@ package pers.ervinse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.UUID;
+
 @SpringBootTest
 class HomeworkManagementSystemApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(UUID.randomUUID());
     }
 
 }
